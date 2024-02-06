7d0c17408ba38f36721cad62bac5dee04c6d9450
Gal Chapman <galchapman@gmail.com>
2024-02-06 18:37:10 +0200

    sleep

---
+++ user/sleep.c
@@ -0,0 +1,14 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+#define STDERR 2
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(STDERR, "usage: %p <seconds>\n", argv[0]);
+    }
+
+    sleep(atoi(argv[1]));
+
+    exit(0);
+}
